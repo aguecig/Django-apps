115013e2993064947c51a35addaf0e85080652e3
Giancarlo <aguecig.9@gmail.com>
2019-08-20 22:59:13 -0400

    removing practise app

--- DataApp/analysis/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class MatrixForm(forms.Form):
-    a1 = forms.DecimalField(label='a1')
-    b1 = forms.DecimalField(label='b1')
-    d1 = forms.DecimalField(label='d1')
-    a2 = forms.DecimalField(label='a2')
-    b2 = forms.DecimalField(label='b2')
-    d2 = forms.DecimalField(label='d2')
--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 # mathapp : my personal website for highschool students for my tutoring job. Has notes, quizzes, and a forum.
 
-# dataapp: personal application that uses a variety of Python libraries to organize data
